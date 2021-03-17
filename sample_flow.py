31caa5b453d05342d5d5e3b56cada2b02a5d0c11
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2021-03-16 21:50:17 -0500

    Update sample_flow.py

--- sample_flow.py
+++ sample_flow.py
@@ -1,7 +1,10 @@
 from prefect import task, Flow
+from prefect.executors import DaskExecutor
+from prefect.storage import GitHub
 import datetime
 import random
 
+import yaml
 
 @task
 def inc(x):
@@ -22,10 +25,21 @@ def add(x, y):
 def list_sum(arr):
     return sum(arr)
 
+with open('dask-worker-spec.yml') as f:
+    worker_config = yaml.load(f, Loader=yaml.FullLoader)
 
 with Flow("test-flow") as flow:
     incs = inc.map(x=range(100))
     decs = dec.map(x=range(100))
     adds = add.map(x=incs, y=decs)
     total = list_sum(adds)
+    flow.executor = DaskExecutor(cluster_class='dask_kubernetes.KubeCluster', 
+                                  cluster_kwargs={'pod_template': worker_config},
+                                  adapt_kwargs={'minimum':2, 'maximum': 3}
+                                )
+    flow.storage = GitHub(
+        repo="adam-d-lewis/sample-flow", path="sample_flow.py"
+    )
 
+if __name__=="__main__":
+    flow.register('Test Project', labels=[])  
